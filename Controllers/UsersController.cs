d5c2cf540174f910eeb7e91fc4146c1b4d3ccf36
ivanr <ivanr@DESKTOP-09U4L3N>
2024-11-15 14:20:16 +0100

    Implement connection to Database

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Vazar.Data;
+
+namespace Vazar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+
+
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Vazar.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        public DbSet<User> Users { get; set; }
+    }
+}
+
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
